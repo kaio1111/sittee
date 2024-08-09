636b48a046e34c10aeaccbd61810aff0313adcb4
Kaio <149404810+kaio1111@users.noreply.github.com>
2024-08-09 08:39:28 -0300

    .1

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-const menuToggle = document.getElementById('mobile-menu');
-const menuNav = document.querySelector('.menu');
-
-menuToggle.addEventListener('click', () => {
-    menuNav.classList.toggle('active');
-    menuToggle.classList.toggle('active');
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('active');
+    
+    // Alternar animações das barras
+    const bars = document.querySelectorAll('.bar');
+    bars[0].classList.toggle('bar1-active');
+    bars[1].classList.toggle('bar2-active');
+    bars[2].classList.toggle('bar3-active');
 });
